2118713d7d1562de43639e4f6c8d1c5ceb26fdd1
Blinker821 <40253582+Blinker821@users.noreply.github.com>
2019-04-09 11:42:06 -0700

    Add files via upload
    Completed the lab

--- lab1.py
+++ lab1.py
@@ -2,14 +2,38 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   
+   if int_list == None:
+      raise ValueError
+   if len(int_list) == 0:
+      return None
+
+   maxnum = int_list[0]
+   for num in int_list:
+      if num > maxnum:
+         maxnum = num
+      
+   return maxnum
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError
+
+   if len(int_list) == 1:
+      return int_list[0:]
+   ans = [int_list[-1]]
+   return ans + reverse_rec(int_list[:len(int_list)-1])
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+   if int_list == None:
+      raise ValueError
+   if not target in int_list[low:high+1]:
+      return None
+
+   if int_list[low] == target:
+      return low
+   return bin_search(target, low+1, high, int_list)
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,23 +1,71 @@
-aimport unittest
+import unittest
 from lab1 import *
 
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
+        """Tests max_list for ValueError"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+    def test_max_list_iter1(self):
+        """Tests max_list for an empty list"""
+        tlist = []
+        self.assertEquals(max_list_iter(tlist), None)
+    def test_max_list_iter2(self):
+        """Tests max_list for a normal list"""
+        tlist = [1,2,3]
+        self.assertEquals(max_list_iter(tlist), 3)
+    def test_max_list_iter3(self):
+        """Tests max_list for another normal list"""
+        tlist = [3, 21, 4]
+        self.assertEquals(max_list_iter(tlist), 21)
 
     def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        """Tests reverse_rec normally"""
+        self.assertEquals(reverse_rec([1,2,3]),[3,2,1])
+    def test_reverse_rec1(self):
+        """Tests reverse_rec normally again"""
+        tlist = [42, 13, 7]
+        self.assertEquals(reverse_rec(tlist), [7,13,42])
+    def test_reverse_rec2(self):
+        """Tests reverse_rec for ValueError"""
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            reverse_rec(tlist)
 
     def test_bin_search(self):
+        """Tests bin_search normally"""
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+    def test_bin_search1(self):
+        """Tests bin_search for upper edge case"""
+        list_val = [12, 34, 92, 6]
+        low = 2
+        high = len(list_val)-1
+        self.assertEqual(bin_search(6, low, high, list_val), 3)
+    def test_bin_search2(self):
+        """Tests bin_search for lower edge case"""
+        list_val = [12, 34, 92, 6]
+        low = 2
+        high = len(list_val)-1
+        self.assertEqual(bin_search(92, low, high, list_val), 2)
+    def test_bin_search3(self):
+        """Tests_bin_search for ValueError"""
+        list_val = None
+        low = 0
+        high = 10
+        with self.assertRaises(ValueError):  # used to check for exception
+            bin_search(92, low, high, list_val)
+    def test_bin_search4(self):
+        """Tests bin_search for target not in list"""
+        list_val = [3,4,5,6]
+        low = 0
+        high = 3
+        self.assertEqual(bin_search(12, low, high, list_val), None)
 
 if __name__ == "__main__":
         unittest.main()
